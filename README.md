1f200e6e6840f8118bb3a6c14a482faf565157de
Nushrat309 <nushratjahan1206@gmail.com>
2025-08-03 02:36:10 +0600

    added authentication and authorization

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # E-Commerce MERN stact project
+
 ## Basic sertup
+
 1.Course plan
 2.Environment setup
-3.CREate Express server
+3.Create Express server
 4.HTTP request & response
+5.nodemon and morgan package
+6.API testing with Postman
+7.Middleware & Types of Middleware
+8.Express Error Handaling Middleware
--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,44 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.get('/',(req, res) => {
+app.use (morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+const isLoggedIn=(req, res, next)=>{
+    const login = true;
+    if (login) {
+        req.body.id = 101;
+        next();
+    } else {
+        return res.status(401).json({message: 'please login first' });
+    }
+};
+
+app.use(isLoggedIn);
+
+app.get('/test', (req,res) => { 
     res.status(200).send({
-        message: 'Welcome to the server made by Nupur!',
+        message:'api testing is working fine',
     });
 });
 
-app.get('/products',(req, res) => {
-    console.log('GET / was called');
+app.get('/api/user', isLoggedIn, (req,res) => { 
+    console.log(req.body.id);
     res.status(200).send({
-        message: 'products are returned',
-    });
+        message:'user profile is returned',});
+});
+//client error handaling
+app.use((req,res,next) => {
+  res.status(404).json({message: 'route not found'});
+  next();
+});
+
+// server error handaling
+app.use((err,req, res, next)=> {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 app.listen(3001,() => {
